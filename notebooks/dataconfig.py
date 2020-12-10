26de5e2737c06078af377df0ad6ede37a5a54b56
Celine Durniak <celine.durniak@esss.se>
2020-12-10 14:42:26 +0100

    Added scipy to environment.yml

---
+++ notebooks/dataconfig.py
@@ -0,0 +1,4 @@
+data_root="/Users/celinedurniak/V20DiffractionData/DENEX/"
+data_mcstas="/Users/celinedurniak/V20DiffractionData/V20_config6/"
+data_he3="/Users/celinedurniak/V20DiffractionData/TsDau/"
+data_extracted_from_root="/Users/celinedurniak/V20DiffractionData/Files_from_ROOT/Spectrum03/"
